15eed5dc4a2d277775b276cfb5cd54bb7d3cfe83
abhay <abhaytaras18@gmail.com>
2021-04-13 18:37:50 +0530

    init

---
+++ sum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+float num1,num2,sum = 0;
+
+sum = num1+num2;
+
+printf("enter number 1 =  ");
+
+scanf("%f",&num1);
+
+printf("\nenter the number 2 = ");
+
+scanf("%f",&num2);
+
+
+printf("\n num1 + num2 = %f ",sum);
+
+return 0;
+ 
+
+
+}
