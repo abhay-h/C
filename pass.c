796b215a4efd8b0c29b56d2fb8f7528101668f4d
abhay <abhaytaras18@gmail.com>
2021-04-13 19:10:01 +0530

    init

---
+++ pass.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+#define password = 1234;
+
+int entry , x = 10;
+
+printf(" welcome ");
+
+while (x!=0)
+{
+    printf("\n enter password :- ");
+    scanf("%d", &entry);
+
+    if (entry == 1234)
+    {
+        printf("\npassword is correct");
+
+        printf("\n your hash is =  5R3QPM756");
+
+        x = 0;
+    
+    }
+    
+    else{
+        printf("\n try again , its worng password");
+
+    }
+    printf("\n");
+}
+
+return 0;
+
+}
