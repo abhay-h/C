67afc76fe79da7b0b4c4500e57d3b54c48011327
abhay <abhaytaras18@gmail.com>
2021-04-16 11:36:53 +0530

    hehe

---
+++ myudf2.c
@@ -0,0 +1,27 @@
+// C program to find  Work, Power, and Energy //
+#include <stdio.h>
+#include <stdlib.h>
+float calculation(int m, int v)
+{
+    return (0.5 * m * v * v);
+}
+int main()
+{
+
+    float m, v, kenergy;
+
+    printf("\nenter the values of Mass =  ");
+
+        scanf("%d", &m);
+
+    printf("\nenter the values of Velocity =  ");
+
+        scanf("%f", &v);
+
+    kenergy = calculation(m,v);
+
+    printf("KINETIC ENERGY = %f J", kenergy);
+
+    return kenergy;
+
+}
